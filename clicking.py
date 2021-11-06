aafeff4901e65f42527bb53462e9de79dfd75a55
KDKftw <ooo.kadoun@gmail.com>
2021-11-06 13:15:02 +0100

    right click, moving

--- clicking.py
+++ clicking.py
@@ -3,5 +3,9 @@ import time
 test = 5+1+3+1+13+11+12+5
 print(test)
 while True:
-    time.sleep(10)
-    p.click()
+    time.sleep(6)
+    p.click(button='right')
+    p.moveTo(1135, 456)
+    time.sleep(15)
+    p.click(button='right')
+    p.moveTo(879, 455)
