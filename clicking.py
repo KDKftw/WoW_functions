7ad6459a791abe36f1625e844e7a585a3f49e46f
KDKftw <ooo.kadoun@gmail.com>
2021-11-08 20:38:39 +0100

    no message

--- clicking.py
+++ clicking.py
@@ -1,5 +1,13 @@
 import pyautogui as p
 import time
+
+def jenKlikej():
+    while True:
+        p.click()
+        time.sleep(6)
+
+jenKlikej()
+
 test = 5+1+3+1+13+11+12+5
 print(test)
 while True:
@@ -9,3 +17,6 @@ while True:
     time.sleep(15)
     p.click(button='right')
     p.moveTo(879, 455)
+
+
+
